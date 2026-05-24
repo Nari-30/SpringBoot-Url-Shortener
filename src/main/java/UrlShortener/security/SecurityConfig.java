9a0807be7c4213d1765b5f6a366276348fd6751a
Sadi Narendhra <narendhrasadi@gmail.com>
2026-05-24 09:17:58 +0530

    Update SecurityConfig.java

--- src/main/java/UrlShortener/security/SecurityConfig.java
+++ src/main/java/UrlShortener/security/SecurityConfig.java
@@ -1,91 +1,31 @@
-package UrlShortener.security;
+package com.example.UrlShortener.config;
 
 import org.springframework.context.annotation.Bean;
-
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
-import org.springframework.security.config.http.SessionCreationPolicy;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import org.springframework.security.crypto.password.PasswordEncoder;
-
 import org.springframework.security.web.SecurityFilterChain;
-
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
-    private final JwtFilter jwtFilter;
-
-    public SecurityConfig(
-            JwtFilter jwtFilter
-    ) {
-
-        this.jwtFilter = jwtFilter;
-    }
-
-    // Password Encoder
     @Bean
-    public PasswordEncoder passwordEncoder(){
-
-        return new BCryptPasswordEncoder();
-    }
-
-    // Security Config
-    @Bean
-    public SecurityFilterChain
-    securityFilterChain(
-            HttpSecurity http
-    ) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                .csrf(csrf -> csrf.disable())
 
-            // Disable CSRF
-            .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/",
+                                "/api/auth/**",
+                                "/api/url/**"
+                        ).permitAll()
 
-            // Stateless JWT
-            .sessionManagement(session ->
-                session.sessionCreationPolicy(
-                    SessionCreationPolicy.STATELESS
+                        .anyRequest().authenticated()
                 )
-            )
 
-            // Public & Protected APIs
-            .authorizeHttpRequests(auth -> auth
-
-                // Public Routes
-                .requestMatchers(
-
-                    "/",
-                    "/index.html",
-                    "/login.html",
-                    "/register.html",
-                    "/dashboard.html",
-
-                    "/style.css",
-                    "/script.js",
-
-                    "/api/auth/**",
-
-                    // Public Redirect URLs
-                    "/r/**"
-
-                ).permitAll()
-
-                // Everything Else Protected
-                .anyRequest()
-                .authenticated()
-            )
-
-            // JWT Filter
-            .addFilterBefore(
-                jwtFilter,
-                UsernamePasswordAuthenticationFilter.class
-            );
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
